bb6e063333d9164c90f23b170ff18d92ff037fbe
asanaflowdesk-cyber <asanaflowdesk@gmail.com>
2026-05-19 11:44:40 +0500

    Delete src directory

--- src/config/tables.js
+++
@@ -1,8 +0,0 @@
-export const TABLES = Object.freeze({
-  posts: 'pulse_posts',
-  profiles: 'pulse_profiles',
-  tags: 'pulse_hashtags',
-  dicts: 'pulse_dictionary_items',
-  materials: 'pulse_materials',
-  logs: 'pulse_logs'
-});
